1b3b6a695aca70a9026276ec78da32276972401e
LaluPrasad M <laluprasad.73515274@gmail.com>
2021-10-02 18:55:20 +0530

    -1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import Header from "./components/Header";
-import Body from "./components/Body";
+// import Header from "./components/Header";
+// import Body from "./components/Body";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Body />
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import "./App.css";
 
-function App() {
+import { connect } from "react-redux";
+
+import Navbar from "./components/Navbar/Navbar";
+import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
+import Algo from "./components/Algo/Algo";
+
+function App({ current }) {
   return (
-    <div className="App">
-      <Header />
-      <Body />
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Products} />
+          <Route exact path="/cart" component={Cart} />
+          {!current ? (
+            <Redirect to="/" />
+          ) : (
+            <Route exact path="/botsdetails/:id" component={Algo} />
+          )}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    current: state.shop.currentItem,
+  };
+};
+
+export default connect(mapStateToProps)(App);
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+import { connect } from "react-redux";
+
+const Navbar = ({ cart }) => {
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    let count = 0;
+    cart.forEach((item) => {
+      count += item.qty;
+    });
+
+    setCartCount(count);
+  }, [cart, cartCount]);
+
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <h2 className="navbar__logo">Dashboard</h2>
+      </Link>
+      <Link to="/cart">
+        <div className="navbar__cart">
+          <h3 className="cart__title">Cart</h3>
+          <div className="cart__counter">{cartCount}</div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    cart: state.shop.cart,
+  };
+};
+
+export default connect(mapStateToProps)(Navbar);
